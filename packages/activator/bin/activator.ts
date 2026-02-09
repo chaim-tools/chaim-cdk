bed7983e514204a4659fa25391b52c00ea45af7e
riverpickles22 <caharnage@gmail.com>
2026-02-09 14:15:57 -0500

    updated npm build and moved activator files to new location

--- packages/activator/bin/activator.ts
+++ packages/activator/bin/activator.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ActivatorStack } from '../lib/activator-stack';
+import { ActivatorStack } from '../src/activator-stack';
 
 const app = new cdk.App();
 
